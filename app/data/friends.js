c928667dc5e05120676bfea2806ca7bcf34a2752
AlexWetzel <wetzela@live.com>
2017-11-03 12:58:31 -0400

    modul working. added new profiles to friends.js. fixed matchmaking logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,69 @@
 var friends = [
 	{
-	  "name":"Ahmed",
-	  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	  "name":"Matthew",
+	  "photo":"https://pbs.twimg.com/media/B83SK5dCIAAsqF9.jpg",
 	  "scores":[
-	      5,
-	      1,
 	      4,
-	      4,
+	      3,
 	      5,
 	      1,
 	      2,
+	      4,
 	      5,
+	      4,
 	      4,
 	      1
 	    ]
-	}	
+	},	
+	{
+	  "name":"Patrick",
+	  "photo":"http://i0.kym-cdn.com/photos/images/newsfeed/001/089/796/3d7.jpg",
+	  "scores":[
+	      1,
+	      1,
+	      2,
+	      4,
+	      5,
+	      3,
+	      5,
+	      5,
+	      5,
+	      1
+	    ]
+	},
+	{
+	  "name":"Woolsworth",
+	  "photo":"https://pbs.twimg.com/profile_images/920140516817297408/rV7u_roF.jpg",
+	  "scores":[
+	      4,
+	      4,
+	      2,
+	      5,
+	      3,
+	      5,
+	      1,
+	      3,
+	      4,
+	      1
+	    ]
+	},
+	{
+	  "name":"Liam",
+	  "photo":"http://i.imgur.com/pZVlVkG.png",
+	  "scores":[
+	      1,
+	      2,
+	      1,
+	      5,
+	      4,
+	      1,
+	      2,
+	      3,
+	      4,
+	      5
+	    ]
+	}
+
 ];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,38 +8,46 @@ module.exports = function(app) {
 
 	app.post("/api", function(req, res) {
 		
-		// app.use(function(req, res, next){
-		//   if (req.body) {
-		    for (var key in req.body) {
-		      if (/\[\]$/.test(key)) {
-		        req.body[key.replace(/\[\]$/, '')] = req.body[key] || [];
-		        delete req.body[key];
-		      }
-		    }
-		//   }
-		//   next();
-		// });
+		//Removes brackets from the object's array key
+	    for (var key in req.body) {
+	      if (/\[\]$/.test(key)) {
+	        req.body[key.replace(/\[\]$/, '')] = req.body[key] || [];
+	        delete req.body[key];
+	      }
+	    }
 
 
-		console.log(req.body)
-		console.log(req.body.scores)
-		// var userScore = req.body.score
-
-		// console.log(userScore);
+		// console.log(req.body)
+		// console.log(req.body.scores)
+		var userScores = req.body.scores
+		var bestScore = 40;
+		var match = 0;
+		console.log(userScores);
 
 		//loop through the friends list
-		// for (var i = 0; i < friends.length; i++) {
-		// 	for (var j = 0; j < 10; j++) {
-				
-		// 	}
-		// }
+		for (var i = 0; i < friends.length; i++) {
+			var matchScore = 0;
+
+			for (var j = 0; j < 10; j++) {
+				var diff = parseInt(friends[i].scores[j]) - parseInt(userScores[j]);
+				matchScore += Math.abs(diff);
+				console.log(matchScore);
+			}
+
+			console.log(matchScore, bestScore);
+
+			if (matchScore < bestScore) {
+				bestScore = matchScore;
+				match = i;
+			}
+		}
 		//compare individual answer scores directly and get the difference for each
 		//add each answer difference together to get a total score
 		//store the first position index in a variable "match"
 		//
 
 
-		res.json(friends[0]);
+		res.json(friends[match]);
 		// friends.push(req.body)
 	});
 };
